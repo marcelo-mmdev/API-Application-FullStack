f6082749fa8e5e5f2a41c295dd914fb48f46e9cf
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-11-17 11:39:51 -0300

    Add axios in application

--- Client-Front/src/pages/produtos/index.tsx
+++ Client-Front/src/pages/produtos/index.tsx
@@ -1,12 +1,36 @@
 "use client";
 
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 import HeaderHome from "@/components/layouts/headerHome";
 import MenuHome from "@/components/layouts/menuHome";
 import MainHome from "@/components/layouts/mainHome";
 import FooterHome from "@/components/layouts/footerHome";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Produtos() {
+  const [dados, setDados] = useState({});
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/marcelo-mmdev")
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/marcelo-mmdev")
+      .then((response) => {
+        setDados(response.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <Grid
@@ -18,21 +42,27 @@ export default function Produtos() {
         color="blackAlpha.700"
         fontWeight="bold"
       >
-        <GridItem pl="2" bg="AZUL.20" area={"header"}>
-          <HeaderHome />
-        </GridItem>
-        <GridItem area={"nav"}>
-          {/* Menu */}
-          <MenuHome />
-        </GridItem>
-        <GridItem area={"main"}>
-          {/* Main */}
-          <MainHome />
-        </GridItem>
-        <GridItem area={"footer"}>
-          {/* Footer */}
-          <FooterHome />
-        </GridItem>
+        {loading ? (
+          <Text>Carregando informações ...</Text>
+        ) : (
+          <>
+            <GridItem pl="2" bg="AZUL.20" area={"header"}>
+              <HeaderHome />
+            </GridItem>
+            <GridItem area={"nav"}>
+              {/* Menu */}
+              <MenuHome />
+            </GridItem>
+            <GridItem area={"main"}>
+              {/* Main */}
+              <MainHome />
+            </GridItem>
+            <GridItem area={"footer"}>
+              {/* Footer */}
+              <FooterHome />
+            </GridItem>
+          </>
+        )}
       </Grid>
     </>
   );
